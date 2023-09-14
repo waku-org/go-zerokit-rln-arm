d9528d416c7de5121c92b1367e735123d4eab0c9
Richard Ramos <info@richardramos.me>
2023-09-14 19:08:28 -0400

    feat: upgrade to zerokit 0.3.4

--- rln/link.go
+++ rln/link.go
@@ -1,7 +1,7 @@
 package rln
 
 /*
-#cgo LDFLAGS:-lrln -ldl -lm
+#cgo LDFLAGS: -lrln -ldl -lm
 #cgo linux,arm LDFLAGS:-L${SRCDIR}/../libs/armv7-linux-androideabi
 #cgo linux,arm64 LDFLAGS:-L${SRCDIR}/../libs/aarch64-unknown-linux-gnu
 
